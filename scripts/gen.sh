528c850167304e97884f5a220f82020fc7ef75a7
Dominik Schmidt <dev@dominik-schmidt.de>
2022-02-01 13:14:22 +0100

    Fix generator invocation

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -2,8 +2,9 @@
 
 rm -rf /transport/*
 
+mkdir /transport/cpp-qt-client
 /usr/local/bin/docker-entrypoint.sh generate \
 	-i https://raw.githubusercontent.com/owncloud/libre-graph-api/main/api/openapi-spec/v0.0.yaml \
 	-g cpp-qt-client \
-	-t /transport/cpp-qt-client \
+	-t /resources/cpp-qt-client \
 	-o /transport/cpp-qt-client
